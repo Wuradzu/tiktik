aa28af914ac68bba834c2a25a658d5a5556f76d9
Wuradzu <savinykhvladislav@gmail.com>
2023-01-09 16:19:03 +0200

    ADD: Navbar, Sidebar, Google Oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Router from 'next/router'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
+
+import {AiOutlineLogout} from 'react-icons/ai'
+import {BiSearch} from 'react-icons/bi'
+import {IoMdAdd} from 'react-icons/io'
+import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
+  const user = false
   return (
-    <div>Navbar</div>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+      <div className="w-[100px] md:w-[130px]">
+        <Image
+        className="cursor-pointer"
+        src={Logo}
+        alt="tiktik logo"
+        layout="responsive"
+        />
+      </div>
+      </Link>
+      <div>Search</div>
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin
+          onSuccess={(res)=>
+            console.log(res)
+          }
+          onError={()=> {console.log('error')}}
+          />
+        )}
+      </div>
+    </div>
   )
 }
 
