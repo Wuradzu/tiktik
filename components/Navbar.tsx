bf05fc0727381e6da750cedae2983d8e8f90de0d
Wuradzu <savinykhvladislav@gmail.com>
2023-01-11 19:55:47 +0200

    ADD: upload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,76 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import Router from 'next/router'
-import { GoogleLogin, googleLogout } from '@react-oauth/google'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Router from "next/router";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
-import {AiOutlineLogout} from 'react-icons/ai'
-import {BiSearch} from 'react-icons/bi'
-import {IoMdAdd} from 'react-icons/io'
-import Logo from '../utils/tiktik-logo.png'
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
+import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-  const user = false
+  const { userProfile, addUser, removeUser } = useAuthStore();
+  console.log(userProfile);
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-      <div className="w-[100px] md:w-[130px]">
-        <Image
-        className="cursor-pointer"
-        src={Logo}
-        alt="tiktik logo"
-        layout="responsive"
-        />
-      </div>
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="tiktik logo"
+            layout="responsive"
+          />
+        </div>
       </Link>
       <div>Search</div>
       <div>
-        {user ? (
-          <div>Logged in</div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className="flex items-center border-2 px-2 md:px-4 text-md font-semibold gap-2">
+                <IoMdAdd className="text-xl" />{" "}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="profile photo"
+                  />
+                </>
+              </Link>
+            )}
+            <button
+            type="button"
+            className="px-2"
+            onClick={() =>{
+              googleLogout()
+              removeUser()
+            }}
+            >
+              <AiOutlineLogout color="red" fontSize={21}/>
+            </button>
+          </div>
         ) : (
           <GoogleLogin
-          onSuccess={(res)=>
-            console.log(res)
-          }
-          onError={()=> {console.log('error')}}
+            onSuccess={(res) => createOrGetUser(res, addUser)}
+            onError={() => {
+              console.log("error");
+            }}
           />
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
